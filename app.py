0f1a0e76e7dc6ed077f579cc5aecef3b83c8cb2d
Junhua9981 <lichu9981@gmail.com>
2022-01-17 20:48:30 +0800

    ffff

--- app.py
+++ app.py
@@ -20,8 +20,8 @@ origins = [
     "https://localhost.tiangolo.com",
     "http://localhost:5501",
     "http://localhost:8080",
-    "http://127.0.0.1:5501/",
-    "https://webprojfrontend.herokuapp.com/"
+    "http://127.0.0.1:5501",
+    "https://webprojfrontend.herokuapp.com"
 ]
 
 app.add_middleware(CORSMiddleware,
